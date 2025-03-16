33c40a704eda811d1001abf0a1b8d940e5b400be
RomanSemywolos <56417828+RomanSemywolos@users.noreply.github.com>
2025-03-16 09:32:05 +0200

    valid

--- db/models/User.js
+++ db/models/User.js
@@ -5,14 +5,38 @@ const User = sequelize.define("Contact", {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "name must be exist"
+      },
+      notEmpty: {
+        msg: "name cannot be empty"
+      }
+    }
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "email must be exist"
+      },
+      notEmpty: {
+        msg: "email cannot be empty"
+      }
+    }
   },
   phone: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "phone must be exist"
+      },
+      notEmpty: {
+        msg: "phone cannot be empty"
+      }
+    }
   },
   favorite: {
     type: DataTypes.BOOLEAN,
